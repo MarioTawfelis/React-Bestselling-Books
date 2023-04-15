4312e7783a9215b030cb0e981d0b234a90589feb
Mario <mario.tawfelis@outlook.com>
2023-04-15 16:08:00 +0100

    Used arrays and map() to populate components

--- React--Bestselling-Books/src/App.jsx
+++ React--Bestselling-Books/src/App.jsx
@@ -1,28 +1,43 @@
 import "./App.css";
 
+
+const bookArray = [
+  {
+    id: 1,
+    img: "https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg",
+    title: "Atomic Habits",
+    author: "James Clear"
+  },
+  {
+    id: 2,
+    img: "https://m.media-amazon.com/images/I/41X65pro2mS._SY291_BO1,204,203,200_QL40_ML2_.jpg",
+    title: "The Wasp Factory",
+    author: "Iain Banks"
+  }
+
+]
+
 function App() {
   return (
     <section className="booklist">
-      <Book
-        img="https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg"
-        title="Atomic Habits"
-        author="James Clear"
-      />
-      <Book
-        img="https://m.media-amazon.com/images/I/41X65pro2mS._SY291_BO1,204,203,200_QL40_ML2_.jpg"
-        title="The Wasp Factory"
-        author="Iain Banks"
-      />
+      {
+        bookArray.map((book) => {
+          const {id, img, title, author} = book
+
+          return (
+            <Book key={id} img={img} title={title} author={author}/>
+          )
+        })}
     </section>
   );
 }
 
-function Book(props) {
+function Book({ img, title, author }) {
   return (
     <article className="book">
-      <img src={props.img} alt={props.title}/>
-      <h2>{props.title}</h2>
-      <h4>{props.author}</h4>
+      <img src={img} alt={title}/>
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   );
 }
