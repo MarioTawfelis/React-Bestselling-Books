269d4aa054ee44280c68ea545196e9957b1d6b9d
Mario <mario.tawfelis@outlook.com>
2023-04-15 15:29:15 +0100

    Used props to pass values to components

--- React--Bestselling-Books/src/App.jsx
+++ React--Bestselling-Books/src/App.jsx
@@ -3,27 +3,26 @@ import "./App.css";
 function App() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img="https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg"
+        title="Atomic Habits"
+        author="James Clear"
+      />
+      <Book
+        img="https://m.media-amazon.com/images/I/41X65pro2mS._SY291_BO1,204,203,200_QL40_ML2_.jpg"
+        title="The Wasp Factory"
+        author="Iain Banks"
+      />
     </section>
   );
 }
 
-function Book() {
-  const title = "Atomic Habits"
-  const author = "James Clear"
+function Book(props) {
   return (
     <article className="book">
-      <img
-        src="https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg"
-        alt="Atomic Habits by James Clear"
-      />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
+      <img src={props.img} alt={props.title}/>
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
     </article>
   );
 }
