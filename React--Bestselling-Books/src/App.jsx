5bfc0cfbd6b261c7c169fbd27823d5afa834a4e3
Mario <mario.tawfelis@outlook.com>
2023-04-17 10:24:18 +0100

    Added event listeners

--- React--Bestselling-Books/src/App.jsx
+++ React--Bestselling-Books/src/App.jsx
@@ -17,9 +17,40 @@ const bookArray = [
 
 ]
 
+const ExampleEvent = () => {
+  const handleChange = (e) => {
+    console.log(e.target)
+  }
+
+  const handleClick = (e) => {
+    console.log(e.target)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("Form submitted", e)
+  }
+
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <h2>Some Form</h2>
+        <input
+          type='text'
+          name='example'
+          onChange={handleChange}
+          style={{margin: '1rem 0'}}/>
+          <button type="submit">Submit</button>
+      </form>
+      <button onClick={handleClick}>Click Me!</button>
+    </section>
+  )
+}
+
 function App() {
   return (
     <section className="booklist">
+      <ExampleEvent/>
       {
         bookArray.map((book) => {
           const {id, img, title, author} = book
