916ddc0fe12d2f01da447189b674f702fdb2bacb
Mario <mario.tawfelis@outlook.com>
2023-04-18 11:39:43 +0100

    Added more books to bookArray

--- React--Bestselling-Books/src/books.jsx
+++ React--Bestselling-Books/src/books.jsx
@@ -10,8 +10,25 @@ const bookArray = [
       img: "https://m.media-amazon.com/images/I/41X65pro2mS._SY291_BO1,204,203,200_QL40_ML2_.jpg",
       title: "The Wasp Factory",
       author: "Iain Banks",
+    },
+    {
+        id: 3,
+        img: "https://m.media-amazon.com/images/I/411yaMigwbL._SX498_BO1,204,203,200_.jpg",
+        title: "The 48 Laws Of Power",
+        author: "Robert Greene",
+    },
+    {
+        id: 4,
+        img: "https://m.media-amazon.com/images/I/411yaMigwbL._SX498_BO1,204,203,200_.jpg",
+        title: "The Midnight Library",
+        author: "Matt Haig",
+    },
+    {
+        id: 6,
+        img: "https://m.media-amazon.com/images/I/41bDls+-bcL._SX328_BO1,204,203,200_.jpg",
+        title: "A Little Life",
+        author: "Hanya Yanagihara",
     }
   
   ]
-
   export default bookArray
