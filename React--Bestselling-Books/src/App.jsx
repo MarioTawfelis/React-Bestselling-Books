e4f0216520cdfcbcd68fb17bec1a2247e0b26d62
Mario <mario.tawfelis@outlook.com>
2023-04-18 09:59:51 +0100

    Passing down book id from App to book

--- React--Bestselling-Books/src/App.jsx
+++ React--Bestselling-Books/src/App.jsx
@@ -6,13 +6,13 @@ const bookArray = [
     id: 1,
     img: "https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg",
     title: "Atomic Habits",
-    author: "James Clear"
+    author: "James Clear",
   },
   {
     id: 2,
     img: "https://m.media-amazon.com/images/I/41X65pro2mS._SY291_BO1,204,203,200_QL40_ML2_.jpg",
     title: "The Wasp Factory",
-    author: "Iain Banks"
+    author: "Iain Banks",
   }
 
 ]
@@ -40,7 +40,6 @@ const ExampleEvent = () => {
           name='example'
           onChange={handleChange}
           style={{margin: '1rem 0'}}/>
-          <button type="submit">Submit</button>
       </form>
       <button onClick={handleClick}>Click Me!</button>
     </section>
@@ -48,28 +47,35 @@ const ExampleEvent = () => {
 }
 
 function App() {
+  const getBook = (id) => {
+    const book = bookArray.find(book => book.id === id)  
+    console.log(book)
+  }
   return (
     <section className="booklist">
-      <ExampleEvent/>
       {
         bookArray.map((book) => {
           const {id, img, title, author} = book
 
           return (
-            <Book key={id} img={img} title={title} author={author}/>
+            <Book key={id} img={img} title={title} author={author} getBook={getBook}/>
           )
         })}
     </section>
   );
 }
 
-function Book({ img, title, author }) {
+function Book({ id, img, title, author, getBook }) {
   return (
-    <article className="book">
-      <img src={img} alt={title}/>
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-    </article>
+    
+      <article className="book">
+        <img src={img} alt={title}/>
+        <h2>{title}</h2>
+        <h4>{author}</h4>
+        <button onClick={() => getBook(id)}>Buy</button>
+      </article>  
+    
+
   );
 }
 
